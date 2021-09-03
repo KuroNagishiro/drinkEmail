bc094a955703217fa52764309be1e5bf6ab82991
KuroNagishiro <74762837+KuroNagishiro@users.noreply.github.com>
2021-09-03 17:36:18 +0800

    Add files via upload

---
+++ testcloud.py
@@ -0,0 +1,53 @@
+# -*- coding: utf8 -*-
+import smtplib
+import time
+import random
+from imgData import arr
+from email.mime.text import MIMEText
+from email.header import Header
+
+
+
+
+def main_handler(mail_msg,subject):
+    # 获取当前时间
+    curTime = time.strftime("%H:%M:%S", time.localtime())
+    #邮件内容
+    # 发送的内容
+    mail_msg = """
+        <p>现在的时间是:%s</p>
+        <h2 style="color:#f00">距离上一次喝水已经过了一小时了，可以暂时停下手中的游戏和工作喝一杯水啦</h2>
+        """ % (curTime)
+    # 生成随机数
+    i = random.randint(0, 3294)
+    # 拼接图片的url
+    imgUrl1 = '<img src="https://steamuserimages-a.akamaihd.net/ugc/'
+    imgUrl2 = arr[i]
+    imgUrl3 = '">'
+    imgUrl = imgUrl1 + imgUrl2 + imgUrl3
+    # 将图片和发送的内容拼接在一起
+    mail_msg = mail_msg + imgUrl
+
+    message = MIMEText(mail_msg, 'html', 'utf-8')
+
+    #发件人名字
+    message['From'] = Header("提醒喝水机器人", 'utf-8')
+    #收件人名字
+    message['To'] = Header("需要喝水的大家❤", 'utf-8')
+    #邮件标题
+    subject = '该喝水啦'
+    message['Subject'] = Header(subject, 'utf-8')
+    #发送方
+    sender = 'xxx@qq.com'
+    #接收方，可以多个
+    receiver = ['xxx@qq.com']
+
+    #使用qq邮箱服务发送邮件
+    smtpObj = smtplib.SMTP_SSL("smtp.qq.com", 465)#此处使用的是qq的smtp服务发送邮件
+    smtpObj.login(sender, "xxx")#此处填入获取到发件人邮箱的授权码
+    smtpObj.sendmail(sender, receiver, message.as_string())
+    smtpObj.quit()
+    print("邮件发送成功")
+
+
+
